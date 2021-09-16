a4a5ebe63f97d404e3acd223d79bb9c74ba5b315
traceannwhite <traceannwhite@gmail.com>
2021-09-15 19:10:41 -0500

    changed port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const morgan = require("morgan");
 const mongoose = require("./db/connection");
 const app = express();
-const { PORT } = process.env || 3000;
+const { PORT } = process.env || 8080;
 
 // import any routers
 
